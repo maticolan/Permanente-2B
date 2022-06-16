8670628b71d60fd2b53f43d09282155f30b71776
maticolan <101749110+maticolan@users.noreply.github.com>
2022-06-15 21:04:11 -0500

    Add files via upload

---
+++ insertion-sort.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+def INSERTION_SORT(A):
+    for j in range(1,len(A)):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and key < A[i]:
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
+    return A
+
+
+n = 25000
+lista = list(range(n))
+random.shuffle(lista)
+
+
+t_inicio = time.time()
+l = INSERTION_SORT(lista)
+t_final = time.time()
+tiempo = t_final - t_inicio
+print(f"El tiempo de ejecución del algoritmo Insertion Sort para un n = {n} fue de:",tiempo)
